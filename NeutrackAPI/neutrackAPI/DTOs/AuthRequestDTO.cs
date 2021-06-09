1655f121e6ae5a66fc756d191caf02fbef5d8bb0
Vincent Adeyemi <vadeyem1@my.westga.edu>
2021-06-09 18:45:03 -0400

    updated model for login request

--- NeutrackAPI/neutrackAPI/DTOs/AuthRequestDTO.cs
+++ NeutrackAPI/neutrackAPI/DTOs/AuthRequestDTO.cs
@@ -1,10 +1,18 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace NeutrackAPI.DTOs
 {
     public class AuthRequestDTO
     {
+        [Required(ErrorMessage ="Email is required")]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$",
+            ErrorMessage = "Email is invalid")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Password is required")]
+        [RegularExpression(@"^(?=.{8,16}$)(?=.*?[a-z])(?=.*?[0-9]).*$",
+            ErrorMessage = "Password must be between 8 - 16 characters and should contain at least one digit.")]
         public string Password { get; set; }
     }
 }
