c215ee8591b462fb38e72ea78cae2de6a4d418b7
LilitKarayan <lkaraya1@my.westga.edu>
2021-06-13 01:16:23 -0500

    Crated toolbar and side-nav components

--- neutrackClientApp/src/app/app.module.ts
+++ neutrackClientApp/src/app/app.module.ts
@@ -1,18 +1,41 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { FormsComponent } from './forms/forms.component';
+import { NutritionistComponent } from './nutritionist/nutritionist.component';
+import { DashboardComponent } from './nutritionist/dashboard/dashboard.component';
+import { PatientsComponent } from './nutritionist/patients/patients.component';
+import { CalculatorComponent } from './nutritionist/calculator/calculator.component';
+import { AccountComponent } from './nutritionist/account/account.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsComponent,
+    NutritionistComponent,
+    DashboardComponent,
+    PatientsComponent,
+    CalculatorComponent,
+    AccountComponent,
+    SideNavComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
@@ -21,6 +44,9 @@ import { environment } from '../environments/environment';
     })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ]
 })
 export class AppModule { }
