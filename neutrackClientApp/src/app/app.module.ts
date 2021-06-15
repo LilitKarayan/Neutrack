aed643939efe1880465f15d52bc7a015b318cb20
LilitKarayan <lkaraya1@my.westga.edu>
2021-06-15 14:11:49 -0500

    Added Patient table in the Patient component

--- neutrackClientApp/src/app/app.module.ts
+++ neutrackClientApp/src/app/app.module.ts
@@ -20,7 +20,6 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 
-
 @NgModule({
   declarations: [
     AppComponent,
