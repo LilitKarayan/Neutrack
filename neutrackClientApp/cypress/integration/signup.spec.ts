b2cb801a7641e85af8b84008d26aebae9075695c
jordan <jbarron8@my.westga.edu>
2021-06-24 23:35:03 -0400

    Need to finish calculator

--- neutrackClientApp/cypress/integration/signup.spec.ts
+++ neutrackClientApp/cypress/integration/signup.spec.ts
@@ -9,6 +9,7 @@ describe('successful scenario tests - signUp', () => {
 
     cy.get('[formcontrolname=email]').should('have.value', '');
     cy.get('[formcontrolname=password]').should('have.value', '');
+    cy.get('[formcontrolname=confirmationPassword]').should('have.value', '');
     cy.get('[formcontrolname=firstName]').should('have.value', '');
     cy.get('[formcontrolname=lastName]').should('have.value', '');
     cy.get('[formcontrolname=dateOfBirth]').should('have.value', '');
@@ -29,7 +30,7 @@ describe('successful scenario tests - signUp', () => {
     })
   });
 
-  it('should have submit button become enabled', () => {
+  it('should have submit button remain disabled - non matching password', () => {
     cy.visit('/signup');
 
     cy.get('[formcontrolname=email]').click().then(() => {
@@ -38,6 +39,10 @@ describe('successful scenario tests - signUp', () => {
 
     cy.get('[formcontrolname=password]').click().then(() => {
       cy.get('[formcontrolname=password]').type('uwG-1234');
+    })
+
+    cy.get('[formcontrolname=confirmationPassword]').click().then(() => {
+      cy.get('[formcontrolname=confirmationPassword]').type('uwg-1234');
     })
 
     cy.get('[formcontrolname=firstName]').click().then(() => {
@@ -56,9 +61,47 @@ describe('successful scenario tests - signUp', () => {
       cy.get('[formcontrolname=phoneNumber]').type('123-456-7890');
     })
 
-    cy.get('[formcontrolname=gender]').click().then(() => {
-      cy.get('[formcontrolname=gender]').type('uwG-1234');
+    // cy.get('#male').check()
+
+    cy.get('[formcontrolname=yearsOfExperience]').click().then(() => {
+      cy.get('[formcontrolname=yearsOfExperience]').type('3');
+    }).then(() => {
+      cy.get('button[type=submit]').should('be.enabled');
     })
+  });
+
+  it('should have submit button become enabled', () => {
+    cy.visit('/signup');
+
+    cy.get('[formcontrolname=email]').click().then(() => {
+      cy.get('[formcontrolname=email]').type('test@test.com');
+    })
+
+    cy.get('[formcontrolname=password]').click().then(() => {
+      cy.get('[formcontrolname=password]').type('uwG-1234');
+    })
+
+    cy.get('[formcontrolname=confirmationPassword]').click().then(() => {
+      cy.get('[formcontrolname=confirmationPassword]').type('uwG-1234');
+    })
+
+    cy.get('[formcontrolname=firstName]').click().then(() => {
+      cy.get('[formcontrolname=firstName]').type('johnny');
+    })
+
+    cy.get('[formcontrolname=lastName]').click().then(() => {
+      cy.get('[formcontrolname=lastName]').type('test');
+    })
+
+    cy.get('[formcontrolname=dateOfBirth]').click().then(() => {
+      cy.get('[formcontrolname=dateOfBirth]').type('2002-12-01');
+    })
+
+    cy.get('[formcontrolname=phoneNumber]').click().then(() => {
+      cy.get('[formcontrolname=phoneNumber]').type('123-456-7890');
+    })
+
+    // cy.get('#male').check()
 
     cy.get('[formcontrolname=yearsOfExperience]').click().then(() => {
       cy.get('[formcontrolname=yearsOfExperience]').type('3');
