0a00fa797e7b43c3d0dea8664156fc514a867cde
jordan <jbarron8@my.westga.edu>
2021-07-05 17:38:32 -0400

    add patient testing complete

---
+++ neutrackClientApp/cypress/helper/patientHelper.ts
@@ -0,0 +1,49 @@
+export class PatientHelper {
+  directToPatient() {
+    cy.get("[routerLink='/patients']").click();
+    cy.get('.addPatient').click();
+  }
+
+  initValues() {
+    cy.get('[formcontrolname=firstName]').should('have.value', '');
+    cy.get('[formcontrolname=lastName]').should('have.value', '');
+    cy.get('[formcontrolname=gender]').should('have.value', null);
+    cy.get('[formcontrolname=dateOfBirth]').should('have.value', '');
+    cy.get('[formcontrolname=email]').should('have.value', '');
+    cy.get('[formcontrolname=phoneNumber]').should('have.value', '');
+  }
+
+  addPatientFilledOut() {
+    cy.get('[formcontrolname=firstName]')
+      .click()
+      .then(() => {
+        cy.get('[formcontrolname=firstName]').type('Jane');
+      });
+
+    cy.get('[formcontrolname=lastName]')
+      .click()
+      .then(() => {
+        cy.get('[formcontrolname=lastName]').type('Doe');
+      });
+
+    cy.get('[formcontrolname=gender]').select('Female');
+
+    cy.get('[formcontrolname=dateOfBirth]')
+      .click()
+      .then(() => {
+        cy.get('[formcontrolname=dateOfBirth]').type('2002-12-01');
+      });
+
+    cy.get('[formcontrolname=email]')
+      .click()
+      .then(() => {
+        cy.get('[formcontrolname=email]').type('janeDoe@hello.com');
+      });
+
+    cy.get('[formcontrolname=phoneNumber]')
+      .click()
+      .then(() => {
+        cy.get('[formcontrolname=phoneNumber]').type('333-333-3333');
+      });
+  }
+}
