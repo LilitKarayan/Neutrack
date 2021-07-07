61864ef3becbaac9f95813d4771b49448ca0b6bc
jordan <jbarron8@my.westga.edu>
2021-07-07 13:26:24 -0400

    delete product e2e complete

--- neutrackClientApp/cypress/integration/nutritionist/products/product-delete.spec.ts
+++ neutrackClientApp/cypress/integration/nutritionist/products/product-delete.spec.ts
@@ -5,5 +5,46 @@ describe('successful scenario tests - delete product', () => {
   const helper = new Helper();
   const productHelper = new ProductHelper();
 
-  
+  it('should have delete button exist', () => {
+    helper.signInStartDashboard();
+    cy.get("[routerLink='/products']").click();
+    productHelper.searchProduct("New Test");
+
+    cy.get('#deleteButton').should('exist');
+  });
+
+  it('should have confirmation box appear', () => {
+    helper.signInStartDashboard();
+    cy.get("[routerLink='/products']").click();
+    productHelper.searchProduct("New Test");
+
+    cy.get('#deleteButton').click();
+    cy.contains('Confirm action');
+  });
+
+  it('should not delete patient', () => {
+    helper.signInStartDashboard();
+    cy.get("[routerLink='/products']").click();
+    productHelper.searchProduct("New Test");
+
+    cy.get('#deleteButton').click();
+    cy.get('#deleteNoButton')
+      .click()
+      .then(() => {
+        cy.contains("New Test");
+      });
+  });
+
+  it('should delete patient', () => {
+    helper.signInStartDashboard();
+    cy.get("[routerLink='/products']").click();
+    productHelper.searchProduct("New Test");
+
+    cy.get('#deleteButton').click();
+    cy.get('#deleteYesButton')
+      .click()
+      .then(() => {
+        cy.get('#deleteButton').should('not.exist');
+      });
+  });
 });
