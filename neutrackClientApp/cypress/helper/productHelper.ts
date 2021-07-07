553731ec6c86e2e5d5d74c7804bdf400080748a3
jordan <jbarron8@my.westga.edu>
2021-07-06 22:05:12 -0400

    product add e2e finished. Beginning spec

---
+++ neutrackClientApp/cypress/helper/productHelper.ts
@@ -0,0 +1,51 @@
+export class ProductHelper {
+  directToProduct() {
+    cy.get("[routerLink='/products']").click();
+    cy.get(".addProduct").click();
+  }
+
+  initValues() {
+    cy.get('[formcontrolname=name]').should('have.value', '');
+    cy.get('[formcontrolname=caloriesPerGram]').should('have.value', 0);
+    cy.get('[formcontrolname=proteinInGrams]').should('have.value', 0);
+    cy.get('[formcontrolname=fatInGrams]').should('have.value', 0);
+    cy.get('[formcontrolname=carbInGrams]').should('have.value', 0);
+  }
+
+  addProductFilledOut() {
+    cy.get('[formcontrolname=name]')
+      .click()
+      .then(() => {
+        cy.get('[formcontrolname=name]').clear();
+        cy.get('[formcontrolname=name]').type('Chips');
+      });
+
+    cy.get('[formcontrolname=caloriesPerGram]')
+      .click()
+      .then(() => {
+        cy.get('[formcontrolname=caloriesPerGram]').clear();
+        cy.get('[formcontrolname=caloriesPerGram]').type('150');
+      });
+
+      cy.get('[formcontrolname=proteinInGrams]')
+      .click()
+      .then(() => {
+        cy.get('[formcontrolname=proteinInGrams]').clear();
+        cy.get('[formcontrolname=proteinInGrams]').type('2');
+      });
+
+    cy.get('[formcontrolname=fatInGrams]')
+      .click()
+      .then(() => {
+        cy.get('[formcontrolname=fatInGrams]').clear();
+        cy.get('[formcontrolname=fatInGrams]').type('10');
+      });
+
+    cy.get('[formcontrolname=carbInGrams]')
+      .click()
+      .then(() => {
+        cy.get('[formcontrolname=carbInGrams]').clear();
+        cy.get('[formcontrolname=carbInGrams]').type('15');
+      });
+  }
+}
---
+++ neutrackClientApp/cypress/integration/nutritionist/products/product-add.spec.ts
@@ -0,0 +1,57 @@
+import { ProductHelper } from '../../../helper/productHelper';
+import { Helper } from '../../../helper/helper';
+
+describe('successful scenario tests - add patient', () => {
+  const helper = new Helper();
+  const productHelper = new ProductHelper();
+
+  it('should have button to add product', () => {
+    helper.signInStartDashboard();
+
+    cy.get("[routerLink='/products']").click();
+    cy.contains('Add a new product');
+  });
+
+  it('should have popup to add product once button clicked', () => {
+    helper.signInStartDashboard();
+
+    productHelper.directToProduct();
+
+    cy.contains('Add new product');
+  });
+
+  it('should have empty fields & add button disabled', () => {
+    helper.signInStartDashboard();
+
+    productHelper.directToProduct();
+
+    productHelper.initValues();
+    cy.get('#addButton').should('have.class', 'button-color-validation');
+  });
+
+  it('should have add button enabled', () => {
+    helper.signInStartDashboard();
+
+    productHelper.directToProduct();
+
+    productHelper.addProductFilledOut();
+    cy.get('#addButton').should('not.have.class', 'button-color-validation');
+  });
+
+  it('should successfully add product', () => {
+    helper.signInStartDashboard();
+
+    productHelper.directToProduct();
+
+    productHelper.addProductFilledOut();
+    cy.get('#addButton')
+      .click()
+      .then(() => {
+        cy.contains('Chips');
+        cy.contains('150');
+        cy.contains('2');
+        cy.contains('10');
+        cy.contains('15');
+      });
+  });
+});
