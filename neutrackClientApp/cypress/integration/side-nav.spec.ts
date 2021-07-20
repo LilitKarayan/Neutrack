0ad8594746d2da7068af8faec05d1811048f18a4
jordan <jbarron8@my.westga.edu>
2021-07-20 17:24:08 -0400

    Side nav tests completed for patient

--- neutrackClientApp/cypress/integration/side-nav.spec.ts
+++ neutrackClientApp/cypress/integration/side-nav.spec.ts
@@ -8,12 +8,14 @@ const helper = new Helper();
     cy.contains('Dashboard');
     cy.contains('Patients');
     cy.contains('Calculator');
+    cy.contains('Products');
+    cy.contains('Recipes');
   });
 
   it('should direct to dashboard', () => {
     helper.signInStartDashboard();
     cy.get("[routerLink='/dashboard']").click().then(() => {
-      cy.contains('List of the active patients');
+      cy.contains('Total Patients');
     });
   });
 
@@ -28,6 +30,48 @@ const helper = new Helper();
     helper.signInStartDashboard();
     cy.get("[routerLink='/calculator']").click().then(() => {
       cy.contains('BMI & Calorie Calculator');
+    });
+  });
+
+  it('should direct to product', () => {
+    helper.signInStartDashboard();
+    cy.get("[routerLink='/products']").click().then(() => {
+      cy.contains('Products');
+    });
+  });
+
+  it('should direct to recipes', () => {
+    helper.signInStartDashboard();
+    cy.get("[routerLink='/recipes']").click().then(() => {
+      cy.contains('Recipes');
+    });
+  });
+
+  it('should contain all sections of the side nav - patient', () => {
+    helper.signInStartDashboardPatient();
+    cy.contains('History');
+    cy.contains('Calculator');
+    cy.contains('Meal Plans');
+  });
+
+  it('should direct to history - patient', () => {
+    helper.signInStartDashboardPatient();
+    cy.get("[routerLink='/history']").click().then(() => {
+      cy.contains('Weight');
+    });
+  });
+
+  it('should direct to calculator - patient', () => {
+    helper.signInStartDashboardPatient();
+    cy.get("[routerLink='/calculator']").click().then(() => {
+      cy.contains('BMI & Calorie Calculator');
+    });
+  });
+
+  it('should direct to meal plans - patient', () => {
+    helper.signInStartDashboardPatient();
+    cy.get("[routerLink='/recipes']").click().then(() => {
+      cy.contains('Meal Plans');
     });
   });
 });
