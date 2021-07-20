06fff18b80069332f6de1d593c60daaa30598b32
jordan <jbarron8@my.westga.edu>
2021-07-20 09:19:17 -0400

    Search tests finished

--- neutrackClientApp/cypress/integration/search.spec.ts
+++ neutrackClientApp/cypress/integration/search.spec.ts
@@ -10,16 +10,34 @@ describe('successful scenario tests - search', () => {
   it('should search for one patient', () => {
     helper.signInStartDashboard();
     cy.get("[routerLink='/patients']").click()
+
+    cy.get("table").find('tr').should('have.length', 3)
+    cy.get("table").should('contain', "Test Testing")
+
     cy.get("#theSearch").click().then(() => {
-        cy.get("#theSearch").type("John Doe")
+        cy.get("#theSearch").type("Janice")
+    })
+
+    cy.get("#theSearchButton").click().then(() => {
+        cy.get("table").find('tr').should('have.length', 2)
+        cy.get("table").should('not.contain', "Test Testing")
     })
   });
 
   it('should search for one product', () => {
     helper.signInStartDashboard();
     cy.get("[routerLink='/products']").click()
+
+    cy.get("table").find('tr').should('have.length', 11)
+    cy.get("table").should('contain', "Abiyuch, raw")
+
     cy.get("#theSearch").click().then(() => {
         cy.get("#theSearch").type("Cornstarch")
+    })
+
+    cy.get("#theSearchButton").click().then(() => {
+        cy.get("table").find('tr').should('have.length', 2)
+        cy.get("table").should('not.contain', "Abiyuch, raw")
     })
   });
 });
