b0c452d49bd1b29d8f631eae08fef5d20b1b8d55
jordan <jbarron8@my.westga.edu>
2021-07-21 09:54:15 -0400

    Patient access tests completed

---
+++ neutrackClientApp/cypress/integration/nutritionist/patients/patient-access.spec.ts
@@ -0,0 +1,38 @@
+import { PatientHelper } from '../../../helper/patientHelper';
+import { Helper } from '../../../helper/helper';
+
+describe('successful scenario tests - view patient', () => {
+    const helper = new Helper();
+    const patientHelper = new PatientHelper();
+
+    it('should have access to actions of patient that belongs to nutritionist', () => {
+        helper.signInStartDashboard();
+        patientHelper.directToPatient();
+
+        cy.get("#theSearch").click().then(() => {
+            cy.get("#theSearch").type("Janice")
+        })
+    
+        cy.get("#theSearchButton").click().then(() => {
+            cy.contains("Janice Doe")
+
+            cy.get("table td:last").find("a").should("have.length", 1)
+            cy.get("table td:last").find("button").should("have.length", 2)
+        })
+    });
+
+    it('should not have access to actions of patient that does not belong to nutritionist', () => {
+        helper.signInStartDashboard();
+        patientHelper.directToPatient();
+
+        cy.get("#theSearch").click().then(() => {
+            cy.get("#theSearch").type("patient")
+        })
+    
+        cy.get("#theSearchButton").click().then(() => {
+            cy.contains("Test Patient")
+            cy.get("table td:last").find("a").should("not.exist")
+            cy.get("table td:last").find("a").should("not.exist")
+        })
+    });
+});
